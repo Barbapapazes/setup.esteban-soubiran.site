f7d320b4d10140db2e7a0f36b1815be412f9406b
barbapapazes <e.soubiran25@gmail.com>
2023-08-24 21:45:38 +0200

    feat: add setup for WSL Ubuntu

---
+++ setup_wsl.sh
@@ -0,0 +1,35 @@
+# This script is used to setup a new Ubuntu WSL environment for web development.
+
+# Update and upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install packages
+sudo apt install -y git
+
+# Install Node.js 20.x
+sudo snap install node --channel=20/stable --classic
+
+# Enable pnpm
+corepack enable
+
+# Install GitHub CLI
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# Install Docker
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce
+
+# Install Docker Compose
+sudo apt install -y docker-compose
+
+# Auto remove unused packages
+sudo apt autoremove -y
